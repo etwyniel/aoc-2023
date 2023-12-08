0c1912c71ea81ab474fc5aa2a72db4ec6a5febfe
Aymeric Beringer <aymericberin@gmail.com>
2023-12-08 09:37:28 +0100

    Optimize day 8

--- src/day08.rs
+++ src/day08.rs
@@ -17,19 +17,32 @@ LR
 XXX = (XXX, XXX)
 ");
 
-fn parse_id(s: &str) -> u64 {
+#[derive(Clone, Copy, Default)]
+struct Id {
+    id: u32,
+    ndx: u32,
+}
+
+#[derive(Clone, Copy, Default)]
+struct MapEntry {
+    id: Id,
+    dests: [Id; 2],
+}
+
+fn parse_id(s: &str) -> u32 {
     s.bytes()
         .map(|b| match b {
             b'A'..=b'Z' => b - b'A',
             b'0'..=b'9' => b - b'0' + 26,
             _ => 0,
-        } as u64)
+        } as u32)
         .fold(0, |acc, n| acc * 36 + n)
 }
 
-fn parse_map(input: impl Iterator<Item = String>) -> HashMap<u64, [u64; 2]> {
-    input
-        .flat_map(|ln| {
+fn parse_map(input: impl Iterator<Item = String>) -> Vec<MapEntry> {
+    let map = input
+        .enumerate()
+        .flat_map(|(i, ln)| {
             let (src, dsts) = ln.split_once(" = ")?;
             let src = parse_id(src);
             let (l, r) = dsts
@@ -39,22 +52,45 @@ fn parse_map(input: impl Iterator<Item = String>) -> HashMap<u64, [u64; 2]> {
                 .map(parse_id)
                 .tuples()
                 .next()?;
-            Some((src, [l, r]))
+            Some((src, (i, [l, r])))
         })
-        .collect()
+        .collect::<HashMap<_, _>>();
+    let mut simplified_map = vec![MapEntry::default(); map.len()];
+    map.iter().enumerate().for_each(|(i, (k, &(pos, [l, r])))| {
+        simplified_map[pos] = MapEntry {
+            id: Id {
+                id: *k,
+                ndx: i as u32,
+            },
+            dests: [
+                Id {
+                    id: l,
+                    ndx: map[&l].0 as u32,
+                },
+                Id {
+                    id: r,
+                    ndx: map[&r].0 as u32,
+                },
+            ],
+        };
+    });
+    simplified_map
 }
 
-fn count_steps<F: Fn(u64) -> bool>(
-    map: &HashMap<u64, [u64; 2]>,
+fn count_steps<F: Fn(u32) -> bool>(
+    map: &[MapEntry],
     directions: &[u8],
-    src: u64,
+    src: usize,
     is_end: F,
 ) -> u64 {
-    let mut pos = src;
+    let mut pos = 0;
+    let mut ndx = src;
     let mut i = 0;
     while !is_end(pos) {
         let dir = directions[i % directions.len()] as usize;
-        pos = map[&pos][dir];
+        let entry = map[ndx].dests[dir];
+        pos = entry.id;
+        ndx = entry.ndx as usize;
         i += 1;
     }
     i as u64
@@ -68,9 +104,10 @@ fn part1(mut input: impl Iterator<Item = String>) -> u64 {
         .for_each(|b| if *b == b'L' { *b = 0 } else { *b = 1 });
     input.next();
     let map = parse_map(input);
-    let src = parse_id("AAA");
+    let src_pos = parse_id("AAA");
     let dst = parse_id("ZZZ");
-    count_steps(&map, &directions, src, |p| p == dst)
+    let src_ndx = map.iter().position(|entry| entry.id.id == src_pos).unwrap();
+    count_steps(&map, &directions, src_ndx, |p| p == dst)
 }
 
 fn gcd(mut a: u64, mut b: u64) -> u64 {
@@ -92,8 +129,9 @@ fn part2(mut input: impl Iterator<Item = String>) -> u64 {
         .for_each(|b| if *b == b'L' { *b = 0 } else { *b = 1 });
     input.next();
     let map = parse_map(input);
-    map.keys()
-        .filter(|&p| p % 36 == 0)
-        .map(|&src| count_steps(&map, &directions, src, |p| p % 36 == 25))
+    map.iter()
+        .enumerate()
+        .filter(|(_, &p)| p.id.id % 36 == 0)
+        .map(|(src, _)| count_steps(&map, &directions, src, |p| p % 36 == 25))
         .fold(0, |acc, n| if acc == 0 { n } else { lcm(acc, n) })
 }
