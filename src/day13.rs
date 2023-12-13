3fe405261d31bb457ac1f5738978fa75bf2a91f5
Aymeric Beringer <aymericberin@gmail.com>
2023-12-13 11:05:42 +0100

    Reduce repetition in day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,5 @@
 use aoc_framework::{
-    grid::{Grid, GridView},
+    grid::{self, Grid},
     point::Point,
     *,
 };
@@ -24,60 +24,58 @@ impl_day!(Day13::{part1, part2}: 2023[13], r"
 #....#..#
 ");
 
+type GridView = grid::GridView<'static, bool, 2>;
+
 fn mirror(line: isize, max: isize) -> impl Iterator<Item = (isize, isize)> {
     (0..line).rev().zip(line..max)
 }
 
-fn find_reflection(g: &GridView<'_, bool, 2>) -> u64 {
-    let mut total = 0;
+fn col_reflects<F: FnMut(bool, bool) -> bool>(g: &GridView, x: isize, mut f: F) -> bool {
     let Point([w, h]) = g.size();
-    total += (1..w)
-        .find(|&x| (0..h).all(|y| mirror(x, w).all(|(l, r)| g[(l, y)] == g[(r, y)])))
-        .unwrap_or(0);
-    total += (1..h)
-        .find(|&y| (0..w).all(|x| mirror(y, h).all(|(l, r)| g[(x, l)] == g[(x, r)])))
-        .unwrap_or(0)
-        * 100;
-    total as u64
+    (0..h).all(|y| mirror(x, w).all(|(l, r)| f(g[(l, y)], g[(r, y)])))
 }
 
-fn find_reflection_smudged(g: &GridView<'_, bool, 2>) -> u64 {
-    let mut total = 0;
+fn row_reflects<F: FnMut(bool, bool) -> bool>(g: &GridView, y: isize, mut f: F) -> bool {
     let Point([w, h]) = g.size();
-    total += (1..w)
-        .find(|&x| {
-            let mut diff = 0;
-            (0..h).all(|y| {
-                mirror(x, w)
-                    .map(|(l, r)| (g[(l, y)], g[(r, y)]))
-                    .all(|(l, r)| {
-                        diff += (l != r) as u8;
-                        diff <= 1
-                    })
-            }) && diff == 1
-        })
-        .unwrap_or(0);
-    total += (1..h)
-        .find(|&y| {
-            let mut diff = 0;
-            (0..w).all(|x| {
-                mirror(y, h)
-                    .map(|(l, r)| (g[(x, l)], g[(x, r)]))
-                    .all(|(l, r)| {
-                        diff += (l != r) as u8;
-                        diff <= 1
-                    })
-            }) && diff == 1
-        })
-        .unwrap_or(0)
-        * 100;
-    total as u64
+    (0..w).all(|x| mirror(y, h).all(|(l, r)| f(g[(x, l)], g[(x, r)])))
 }
 
-fn solve<F: Fn(&GridView<'_, bool, 2>) -> u64>(
-    mut input: impl Iterator<Item = String>,
-    f: F,
-) -> u64 {
+fn find_reflection(g: &GridView) -> u64 {
+    let Point([w, h]) = g.size();
+    let horizontal = (1..w)
+        .find(|&x| col_reflects(g, x, |l, r| l == r))
+        .unwrap_or(0);
+    let vertical = (1..h)
+        .find(|&y| row_reflects(g, y, |l, r| l == r))
+        .unwrap_or(0);
+    (horizontal + vertical * 100) as u64
+}
+
+fn reflects_smudged<const HORIZONTAL: bool>(g: &GridView, axis: isize) -> bool {
+    let mut diff = 0;
+    let predicate = |l, r| {
+        diff += (l != r) as u8;
+        diff <= 1
+    };
+    (if HORIZONTAL {
+        col_reflects(g, axis, predicate)
+    } else {
+        row_reflects(g, axis, predicate)
+    }) && diff == 1
+}
+
+fn find_reflection_smudged(g: &GridView) -> u64 {
+    let Point([w, h]) = g.size();
+    let horizontal = (1..w)
+        .find(|&x| reflects_smudged::<true>(g, x))
+        .unwrap_or(0);
+    let vertical = (1..h)
+        .find(|&y| reflects_smudged::<false>(g, y))
+        .unwrap_or(0);
+    (horizontal + vertical * 100) as u64
+}
+
+fn solve<F: Fn(&GridView) -> u64>(mut input: impl Iterator<Item = String>, f: F) -> u64 {
     let mut total = 0;
     loop {
         let mut data = Vec::new();
