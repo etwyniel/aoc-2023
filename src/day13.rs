805b7cdadd6da65970f605ba52feb6b31c0c3ebf
Aymeric Beringer <aymericberin@gmail.com>
2023-12-13 10:28:14 +0100

    Clean up and optimize day 13

--- src/day13.rs
+++ src/day13.rs
@@ -24,95 +24,60 @@ impl_day!(Day13::{part1, part2}: 2023[13], r"
 #....#..#
 ");
 
+fn mirror(line: isize, max: isize) -> impl Iterator<Item = (isize, isize)> {
+    (0..line).rev().zip(line..max)
+}
+
 fn find_reflection(g: &GridView<'_, bool, 2>) -> u64 {
     let mut total = 0;
     let Point([w, h]) = g.size();
-    'outer: for x in 1..w {
-        for y in 0..h {
-            if !(0..x)
-                .rev()
-                .zip(x..w)
-                .all(|(l, r)| g[Point([l, y])] == g[Point([r, y])])
-            {
-                continue 'outer;
-            }
-        }
-        total += x;
-        break;
-    }
-    'outer: for y in 1..h {
-        for x in 0..w {
-            if !(0..y)
-                .rev()
-                .zip(y..h)
-                .all(|(l, r)| g[Point([x, l])] == g[Point([x, r])])
-            {
-                continue 'outer;
-            }
-        }
-        total += y * 100;
-        break;
-    }
+    total += (1..w)
+        .find(|&x| (0..h).all(|y| mirror(x, w).all(|(l, r)| g[(l, y)] == g[(r, y)])))
+        .unwrap_or(0);
+    total += (1..h)
+        .find(|&y| (0..w).all(|x| mirror(y, h).all(|(l, r)| g[(x, l)] == g[(x, r)])))
+        .unwrap_or(0)
+        * 100;
     total as u64
 }
 
 fn find_reflection_smudged(g: &GridView<'_, bool, 2>) -> u64 {
     let mut total = 0;
     let Point([w, h]) = g.size();
-    'outer: for x in 1..w {
-        let mut smudge_found = false;
-        for y in 0..h {
-            if !(0..x)
-                .rev()
-                .zip(x..w)
-                .map(|(l, r)| (g[Point([l, y])], g[Point([r, y])]))
-                .all(|(l, r)| {
-                    if !smudge_found && l != r {
-                        smudge_found = true;
-                        true
-                    } else {
-                        l == r
-                    }
-                })
-            {
-                continue 'outer;
-            }
-        }
-        if smudge_found {
-            total += x;
-            break;
-        }
-    }
-    let Point([w, h]) = g.size();
-    'outer: for y in 1..h {
-        let mut smudge_found = false;
-        for x in 0..w {
-            if !(0..y)
-                .rev()
-                .zip(y..h)
-                .map(|(l, r)| (g[Point([x, l])], g[Point([x, r])]))
-                .all(|(l, r)| {
-                    if !smudge_found && l != r {
-                        smudge_found = true;
-                        true
-                    } else {
-                        l == r
-                    }
-                })
-            {
-                continue 'outer;
-            }
-        }
-        if smudge_found {
-            total += y * 100;
-            break;
-        }
-    }
+    total += (1..w)
+        .find(|&x| {
+            let mut diff = 0;
+            (0..h).all(|y| {
+                mirror(x, w)
+                    .map(|(l, r)| (g[(l, y)], g[(r, y)]))
+                    .all(|(l, r)| {
+                        diff += (l != r) as u8;
+                        diff <= 1
+                    })
+            }) && diff == 1
+        })
+        .unwrap_or(0);
+    total += (1..h)
+        .find(|&y| {
+            let mut diff = 0;
+            (0..w).all(|x| {
+                mirror(y, h)
+                    .map(|(l, r)| (g[(x, l)], g[(x, r)]))
+                    .all(|(l, r)| {
+                        diff += (l != r) as u8;
+                        diff <= 1
+                    })
+            }) && diff == 1
+        })
+        .unwrap_or(0)
+        * 100;
     total as u64
 }
 
-#[aoc(part = 1, example = 405)]
-fn part1(mut input: impl Iterator<Item = String>) -> u64 {
+fn solve<F: Fn(&GridView<'_, bool, 2>) -> u64>(
+    mut input: impl Iterator<Item = String>,
+    f: F,
+) -> u64 {
     let mut total = 0;
     loop {
         let mut data = Vec::new();
@@ -124,25 +89,17 @@ fn part1(mut input: impl Iterator<Item = String>) -> u64 {
         if data.is_empty() {
             break;
         }
-        total += find_reflection(Grid::from_data(data, stride).as_ref());
+        total += f(Grid::from_data(data, stride).as_ref());
     }
     total
 }
 
+#[aoc(part = 1, example = 405)]
+fn part1(input: impl Iterator<Item = String>) -> u64 {
+    solve(input, find_reflection)
+}
+
 #[aoc(part = 2, example = 400)]
-fn part2(mut input: impl Iterator<Item = String>) -> u64 {
-    let mut total = 0;
-    loop {
-        let mut data = Vec::new();
-        let mut stride = 0;
-        (&mut input).take_while(|ln| !ln.is_empty()).for_each(|ln| {
-            stride = ln.len();
-            data.extend(ln.bytes().map(|b| b == b'#'))
-        });
-        if data.is_empty() {
-            break;
-        }
-        total += find_reflection_smudged(Grid::from_data(data, stride).as_ref());
-    }
-    total
+fn part2(input: impl Iterator<Item = String>) -> u64 {
+    solve(input, find_reflection_smudged)
 }
